c295f1c6bd5fdea5813a1959eb1a81ec120d1b3e
alswlfl29 <budlalswl1129@gmail.com>
2024-01-30 09:11:22 +0900

    tailwindcss수업

---
+++ tw/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
